62c69b098f63621f05ef09a123af06eebc327caf
Duc Quach <duc@cofixel.com>
2020-07-30 22:57:40 +0700

    UPDATE: - Create Order event publishers

--- orders/src/events/publishers/ticket-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, Subjects, TicketCreatedEvent } from '@duc.q/common';
-
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
