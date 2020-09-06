1d9380e5471dc0a17ba10925532490a1fb08026f
Duc Quach <duc@cofixel.com>
2020-09-06 23:10:21 +0700

    UPDATE: - Add Expiration service - Update common module

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, OrderCreatedEvent, Subjects } from '@duc.q/common';
+import { queueGroupName } from './queue-group-name';
+import { expirationQueue } from '../../queues/expiration-queue'
+
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    // Times in milisecond
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log(`Waiting this many miliseconds to process the job`, delay)
+
+
+    await expirationQueue.add({
+      orderId: data.id,
+    }, {
+      delay,
+    });
+
+    msg.ack();
+  }
+}
+
--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -1,5 +1,5 @@
 import { natsWrapper } from './nats-wrapper'
-
+import { OrderCreatedListener } from './events/listeners/order-created-listener';
 
 const start = async () => {
   if (!process.env.NATS_CLIENT_ID) {
@@ -27,6 +27,7 @@ const start = async () => {
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
 
+    new OrderCreatedListener(natsWrapper.client).listen();
     console.log("Expiration:SETUP SUCCESSFULLY");
   } catch (error) {
     console.error(error);
