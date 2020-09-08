edf6e1a0bed20d68927c6ccda388bd0780b9f259
Duc Quach <duc@cofixel.com>
2020-09-08 23:30:54 +0700

    UPDATE: -Setup k8s and skaffold - Setup Payments files

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -3,10 +3,6 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@duc.q/common";
-import { createTicketRouter } from './routes/new';
-import { showTicketRouter } from './routes/show';
-import { indexTicketRouter } from './routes/index';
-import { updateTicketRouter } from './routes/update';
 
 const app = express();
 
@@ -17,11 +13,6 @@ app.use(cookieSession({
   secure: process.env.NODE_ENV !== 'test',
 }));
 app.use(currentUser);
-
-app.use(showTicketRouter);
-app.use(createTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
 
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
