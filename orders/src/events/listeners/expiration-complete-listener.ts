414ac87b3419bbd8d9e3e67ffad7049684498228
Duc Quach <duc@cofixel.com>
2020-09-12 17:24:20 +0700

    UPDATE: - Add PaymentCreatedListener event

--- orders/src/events/listeners/expiration-complete-listener.ts
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -19,7 +19,6 @@ export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent
       status: OrderStatus.Cancelled,
     });
 
-    console.log(`ExpirationCompleteListener -> onMessage -> order`, order)
     if (order.status === OrderStatus.Complete) {
       return msg.ack();
     }
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
 import { Order } from "../models/order";
 import { stripe } from '../stripe';
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -43,10 +45,16 @@ router.post(
       orderId,
       stripeId: charge.id,
     });
+
     await payment.save();
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    })
 
     res.status(201).send({
-      success: true,
+      id: payment.id,
     })
   }
 );
