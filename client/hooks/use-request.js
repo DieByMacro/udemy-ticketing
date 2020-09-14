36958164ae06022673d3e153d7ecf0f0d8f4b82e
Duc Quach <duc.quach@groovetechnology.com>
2020-09-14 17:09:28 +0700

    UPDATE: - Update useRequest to merge new objects into body

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -1,25 +1,30 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
-export default ({ url, method, body, onSuccess}) => {
+export default ({ url, method, body, onSuccess }) => {
   const [errors, setErrors] = useState(null);
 
-  const doRequest = async () => {
+  const doRequest = async (props = {}) => {
     try {
       setErrors(null);
-      const response = await axios[method](url,body);
+      const response = await axios[method](url, {
+        ...body,
+        ...props,
+      });
 
       if (onSuccess) {
         onSuccess(response.data);
       }
-      
+
       return response.data;
     } catch (err) {
       setErrors(
         <div className="alert alert-danger">
           <h4>Errors</h4>
           <ul className="my-0">
-            {err.response.data.errors.map( err => <li key={err.message}>{err.message}</li>)}
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
           </ul>
         </div>
       );
@@ -28,6 +33,6 @@ export default ({ url, method, body, onSuccess}) => {
 
   return {
     doRequest,
-    errors
-  }
-}
+    errors,
+  };
+};
