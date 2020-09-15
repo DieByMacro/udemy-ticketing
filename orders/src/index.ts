beadb3595c538e78a8b827ec6a374a435698bd2f
Duc Quach <duc@cofixel.com>
2020-09-15 23:39:01 +0700

    UPDATE: -Testing workflow 2

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -8,6 +8,8 @@ import { PaymentCreatedListener } from './events/listeners/payment-created-liste
 
 const start = async () => {
   console.log('Testing workflows');
+  console.log('Testing workflows');
+  console.log('Testing workflows');
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
